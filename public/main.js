a8486b718ea1f9d7c4dfaee82e651da1f80eb368
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-05-17 13:37:22 -0400

    all api done!

--- public/main.js
+++ public/main.js
@@ -1,5 +1,4 @@
 const main = () => {
-  document.querySelector('.pic-of-the-day').textContent = ' '
   fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
     .then(resp => {
       console.log(resp)
@@ -10,6 +9,24 @@ const main = () => {
       // update the HTML
       document.querySelector('.pic-of-the-day').style.backgroundImage =
         'url(' + message.url + ')'
+      fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
+        .then(resp => {
+          console.log(resp)
+          return resp.json()
+        })
+        .then(upcomingLaunches => {
+          console.log(upcomingLaunches)
+          // update the HTML
+          document.querySelector('.name-of-launch').textContent =
+            upcomingLaunches[0].mission_name
+          document.querySelector('.launch-info').textContent =
+            upcomingLaunches[0].details
+          document.querySelector('.launch-time').textContent =
+            upcomingLaunches[0].launch_date_unix
+          document.querySelector('.launch-place').textContent =
+            upcomingLaunches[0].launch_site.site_name_long
+        })
     })
 }
+
 document.addEventListener('DOMContentLoaded', main)
