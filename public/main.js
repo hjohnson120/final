dd2ec1312d0493061c7f6fdf0029ba83b8caa0c0
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-05-17 10:04:49 -0400

    working on pic

--- public/main.js
+++ public/main.js
@@ -1,7 +1,15 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  document.querySelector('.pic-of-the-day').textContent = ' '
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(resp => {
+      console.log(resp)
+      return resp.json()
+    })
+    .then(message => {
+      console.log(message)
+      // update the HTML
+      document.querySelector('.pic-of-the-day').style.backgroundImage =
+        'url(' + message.url + ')'
+    })
 }
-
 document.addEventListener('DOMContentLoaded', main)
